89ad30abcf4c925c31bd72614716c6a701875c44
Mehedi-Hasan-18 <mdmehedihasanroby@gmail.com>
2025-05-15 16:07:39 +0800

    FILTER,SEARCH,PAGINATION

--- product/views.py
+++ product/views.py
@@ -2,10 +2,19 @@ from product.models import Product,Category,Review
 from product.serializer import ProductSerializer,CategorySerializer,ReviewSerializer
 from django.db.models import Count
 from rest_framework.viewsets import ModelViewSet
+from django_filters.rest_framework import DjangoFilterBackend
+from product.filters import ProductFilter
+from rest_framework.filters import SearchFilter,OrderingFilter
+from product.paginations import DefaulfPagination
 
 class ProductViewSets(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    filter_backends = [DjangoFilterBackend,SearchFilter,OrderingFilter]
+    pagination_class = DefaulfPagination
+    filterset_class = ProductFilter
+    search_fields = ['name','description']
+    ordering_fields = ['price','updated_at']
     
 class CategoryViewSet(ModelViewSet):
     queryset = Category.objects.annotate(product_count = Count("products")).all()
