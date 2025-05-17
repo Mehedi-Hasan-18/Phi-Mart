2de43d12abdd75c96e9ba2b62975c7359ca66980
Mehedi-Hasan-18 <mdmehedihasanroby@gmail.com>
2025-05-18 01:36:32 +0800

    cartapi implemet done

--- api/urls.py
+++ api/urls.py
@@ -2,17 +2,23 @@ from django.urls import path,include
 from rest_framework.routers import DefaultRouter
 from product.views import ProductViewSets,CategoryViewSet,ReviewViewSet
 from rest_framework_nested import routers
+from order.views import CartViewSet,CartItemViewSet
 
 router = routers.DefaultRouter()
 router.register('products',ProductViewSets)
 router.register('categories',CategoryViewSet)
+router.register('carts',CartViewSet,basename='carts')
 
 product_router = routers.NestedDefaultRouter(router,'products', lookup='product')
 product_router.register('review',ReviewViewSet,basename='product-review')
+
+cart_router = routers.NestedDefaultRouter(router,'carts',lookup='cart')
+cart_router.register('items',CartItemViewSet,basename='cart-item')
 
 # urlpatterns = router.urls
 
 urlpatterns = [
     path('',include(router.urls)),
     path('',include(product_router.urls)),
+    path('',include(cart_router.urls)),
 ]
--- order/models.py
+++ order/models.py
@@ -1,18 +1,25 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 from users.models import User
 from product.models import Product
+from uuid import uuid4
 
 class Cart(models.Model):
+    id = models.UUIDField(primary_key=True, editable=False,default=uuid4)
     user = models.OneToOneField(User,on_delete=models.CASCADE,related_name='cart')
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f'Cart Of {self.user.username}' 
+        return f'Cart Of {self.user.first_name}' 
     
 class CartItem(models.Model):
     cart = models.ForeignKey(Cart,on_delete=models.CASCADE,related_name='items')
     product = models.ForeignKey(Product,on_delete=models.CASCADE)
-    quantity = models.PositiveIntegerField()
+    quantity = models.PositiveIntegerField(validators=[MinValueValidator(1)])
+    
+    class Meta:
+        unique_together = [['cart','product']]
+        
     
     def __str__(self):
         return f'{self.quantity} X {self.product.name}'
